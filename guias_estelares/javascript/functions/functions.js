dda189bb5619d307dafcde3141b8c97b5756a55a
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-07 07:07:29 -0300

    Update functions.js
    Visto a passagem de valores por valor e por referência.

--- guias_estelares/javascript/functions/functions.js
+++ guias_estelares/javascript/functions/functions.js
@@ -18,10 +18,59 @@ console.log('Fim do programa')
 // -----------------------------------------------------------------------
 // Function expression or function anonymous
 // parâmetros (Parameters)
+
+// const sum = function(number1, number2) {
+//     console.log(number1 + number2)
+// }
+
+// sum(2, 3) //Passando arguments - argumentos
+// sum(25, 17)
+
+// -----------------------------------------------------------------------
+
 const sum = function(number1, number2) {
-    console.log(number1 + number2)
+    // Nunca criar uma variavel sem o operador especial
+    // total = number1 + number2
+    let total = number1 + number2
+    return total
 }
 
-sum(2, 3) //Passando arguments - argumentos
+let number3 = 34
+let number4 = 25
 
-// ---------------------------------------------------------------------
+console.log(`O número 1 é ${number3}`)
+console.log(`O número 2 é ${number4}`)
+console.log(`A soma é ${sum(number3, number4)}`)
+
+// console.log(total)
+
+// -----------------------------------------------------------------------
+
+function fazerSuco(fruta1, fruta2) {
+    return fruta1 + fruta2
+}
+
+const copo = fazerSuco('limão', 'abacaxi')
+
+console.log(copo)
+
+// -----------------------------------------------------------------------
+// function scope
+let subject = 'create video'
+// Passando variavel por valor
+function createThink(subject) {
+    subject = 'study'
+    return subject
+}
+
+console.log(createThink(subject))
+console.log(subject)
+
+// Passando variavel por referência
+function createThink2() {
+    subject = 'black'
+    return subject
+}
+
+console.log(createThink2())
+console.log(subject)
