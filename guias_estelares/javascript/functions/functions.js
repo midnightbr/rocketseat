c75827ff75dd3db9a8a2d0857230b7647f77cfb0
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-06 21:35:11 -0300

    Functions JS
    Inicio das aulas sobre o tema de funções no JS

---
+++ guias_estelares/javascript/functions/functions.js
@@ -0,0 +1,27 @@
+// Criar um aplicativo de frases motivacionais
+
+// Declaration - declaração da função
+// Function statement
+function createPhrases() {
+    console.log('Estudar é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')
+}
+
+// Executar a função
+// Rodar, chamar, invocar
+// Execute, run, call, invoke
+createPhrases()
+
+console.log('Fim do programa')
+
+// -----------------------------------------------------------------------
+// Function expression or function anonymous
+// parâmetros (Parameters)
+const sum = function(number1, number2) {
+    console.log(number1 + number2)
+}
+
+sum(2, 3) //Passando arguments - argumentos
+
+// ---------------------------------------------------------------------
