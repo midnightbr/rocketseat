cd448b3b7c5998a6157a74964450d3481f220f29
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-08 07:13:02 -0300

    Conceitos básicos de JS
    Começo das aulas sobre prototype e manipulação de dados

---
+++ guias_estelares/javascript/prototype/script.js
@@ -0,0 +1,11 @@
+/*
+    Prototype
+    
+    * prototype-based language
+    * prototype chain
+    * __proto__
+*/
+
+// para testar o prototype digite no debugger do navegador
+// "Marcos".__proto__
+// 28.0.__proto__
