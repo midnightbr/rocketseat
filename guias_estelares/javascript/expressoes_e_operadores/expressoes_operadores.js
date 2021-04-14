b7a4dc3683821f61d624c3d8b397be0a7368c7d2
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-14 07:15:16 -0300

    Update expressoes_operadores.js
    Continuação da aulas sobre expressões e operadores no JS.

--- guias_estelares/javascript/expressoes_e_operadores/expressoes_operadores.js
+++ guias_estelares/javascript/expressoes_e_operadores/expressoes_operadores.js
@@ -42,3 +42,85 @@ console.log(name, age)
 
 let date = new Date('2021-04-13')
 console.log(date)
+
+/*
+    Operadores unários
+        typeof
+        delete
+ */
+
+//typeof, usado para descobrir o tipo de algum objeto
+console.log(typeof age)
+
+// delete
+const person = {
+    name: "Marcos",
+    age: 24,
+    Student: true,
+}
+console.log(person)
+// delete vai apagar a variavel de dentro do objeto person
+delete person.age
+console.log(person)
+console.log(typeof person)
+
+// Operadores Aritiméticos
+// multiplicação
+console.log(3.2 * 5.5)
+// divisão
+console.log(12 / 2)
+// soma
+console.log(34 + 67)
+// subtração
+console.log(34 - 23)
+// resto da divisão
+let remainder
+remainder = 11 % 3
+console.log(remainder)
+// incremento
+let increment = 0
+increment++
+console.log(increment++)
+console.log(increment)
+console.log(++increment)
+// decremento
+let decrement = 5
+console.log(--decrement)
+console.log(decrement--)
+console.log(decrement)
+// exponencial
+console.log(3 ** 3)
+console.log(5 ** 2)
+
+// Grouping operator ()
+let total = 2 + 3 * 5
+console.log(total)
+total = (2 + 3) * 5
+console.log(total)
+
+// Operadores de Comparação
+// Irá comparar valores e retornar um Boolean como resposta á comparação
+
+let one = 1
+let two = 2
+
+// == igual
+console.log(one == two) //false
+console.log(one == "1") //true
+// != diferente
+console.log(one != two) //true
+console.log(two != "2") //false
+// === estritamente igual
+console.log(one === two) //false
+console.log(one === "1") //false
+// !== estritamente diferente
+console.log(one !== two) //true
+console.log(two !== "2") //true
+// > Maior
+console.log(one > two) //false
+// >= Maior igual
+console.log(one >= two) //false
+// < Menor
+console.log(one < two) //true
+// <= Menos igual
+console.log(one <= two) //true
