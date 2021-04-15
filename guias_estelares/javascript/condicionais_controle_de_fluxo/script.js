8b4d89028bfb977756a2fcdb9b55c851f4e21ccf
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-15 07:36:59 -0300

    Condicionais e controle de fluxo
    Começo das aulas sobre o controle de fluxo e condicionais if e else, como também o switch case.

---
+++ guias_estelares/javascript/condicionais_controle_de_fluxo/script.js
@@ -0,0 +1,53 @@
+// if...else
+// Opção 01 - Funciona, mais um pouco bagunçado
+let temperature = 36.5
+if(temperature >= 37.5) {
+    console.log('Febre alta')
+} else if(temperature < 37.5 && temperature >= 37) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+
+// Opção 02 - Funciona e mais organizado. Recomendado
+temperature = 37
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+if(highTemperature) {
+    console.log('Febre alta')
+} else if(mediumTemperature) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+
+// Switch
+function calculate(number1, operator, number2) {
+    let result
+
+    switch (operator) {
+        case '+':
+            result = number1 + number2
+            break;
+        case '-':
+            result = number1 - number2
+            break;
+        case '*':
+            result = number1 * number2
+            break;
+        case '/':
+            result = number1 / number2
+            break;
+        default:
+            console.log('Não implementado')
+            break;
+    }
+
+    return result
+}
+
+console.log(calculate(4, '+', 8))
+console.log(calculate(5, '-', 2))
+console.log(calculate(10, '*', 12))
+console.log(calculate(15, '/', 5))
+console.log(calculate(12, '^^', 2))
