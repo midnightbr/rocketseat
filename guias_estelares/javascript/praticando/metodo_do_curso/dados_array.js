322c60cd8cae735176db461a4bca6613ab10390d
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-18 00:56:39 -0300

    Create dados_array.js
    Método do curso para a solução da questão do desafio

---
+++ guias_estelares/javascript/praticando/metodo_do_curso/dados_array.js
@@ -0,0 +1,13 @@
+/*
+    Buscando e contando dados em Arrays
+
+    Baseado no Array de Livros por Categoria abaixo, 
+    faça os seguintes desafios:
+
+        * Contar o número de categorias e o número de Livros
+        em cada categoria
+        * Contar o número de autores
+        * Mostrar livros do autor Augusto Cury
+        * Transformar a função acima em uma função que irá receber
+        o nome do autor e devolver os livros desse autor.
+*/
