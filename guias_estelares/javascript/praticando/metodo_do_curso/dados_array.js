afdba14dcede4f257c90e3b3cc9a1c160a0245ec
midnightbr <80632969+midnightbr@users.noreply.github.com>
2021-04-18 22:55:11 -0300

    Método de solução do curso
    Forma do curso de resolver o desafio. Ficou bem mais simples e mais pratico do que o meu. Tive bastante dificuldade em tentar acessar os livros e os nomes do autores dentro do array. Mais fica aqui uma dica bastante importante que vou rever bastante.
    Além do outro fato, ele resolveu cada questão por partes, enquanto quis resolver todos de uma vez só.

--- guias_estelares/javascript/praticando/metodo_do_curso/dados_array.js
+++ guias_estelares/javascript/praticando/metodo_do_curso/dados_array.js
@@ -11,3 +11,89 @@
         * Transformar a função acima em uma função que irá receber
         o nome do autor e devolver os livros desse autor.
 */
+
+const booksByCategory = [
+    {
+        category: 'Riqueza',
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: 'Inteligência Emocional',
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+
+const totalCategories = booksByCategory.length
+console.log(totalCategories)
+for(let category of booksByCategory) {
+    console.log('Total de livros da categoria: ', category.category)
+    console.log(category.books.length)
+}
+
+function countAuthors() {
+    let authors = []
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+    console.log('Total de autores: ', authors.length)
+}
+countAuthors()
+
+function booksOfAugustoCury() {
+    let books = []
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === 'Augusto Cury') {
+                books.push(book.title)
+            }
+        }
+    }
+    console.log('Livros do autor: ', books)
+}
+booksOfAugustoCury()
+
+function booksOfAuthor(author) {
+    let books = []
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if(book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+    console.log(`Livros do autor ${author}: , ${books.join(", ")}`)
+}
+booksOfAuthor('George S. Clason')
